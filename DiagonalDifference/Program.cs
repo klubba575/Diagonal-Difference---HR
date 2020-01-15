e5ef8407454213700c081a5b29a3f4d6298053a1
klubba575 <wilderandrew9@gmail.com>
2020-01-15 13:38:45 -0500

    Solution to Hacker Rank Exercise

---
+++ DiagonalDifference/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DiagonalDifference
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = 3;
+			int[,] arr = { { 12, 5, 3 }, { 7, 1, 23 }, { 4, 9, 5 } };
+			Console.WriteLine(difference(arr, n));
+			
+			static int difference(int [,] arr, int n)
+			{
+				int d1 = 0;
+				int d2 = 0;
+				for (int a = 0; a < n; a++)
+				{
+					for (int b = 0; b < n; b++)
+					{
+						if (a == b)
+						{
+							d1 += arr[a, b];
+						}
+						if (a  == n - b - 1)
+						{
+							d2 += arr[a, b];
+						}
+					}
+				}
+				return Math.Abs(d1 - d2);
+			}
+			
+		}
+	}
+}
